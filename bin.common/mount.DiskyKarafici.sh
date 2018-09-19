a4175be23c8b14879e2f1a9ac9ed31ecb1583119
Ja from Lenovo <karafi@gmail.com>
2018-09-19 09:16:38 +0200

    add mount.DiskyKarafici.sh

---
+++ bin.common/mount.DiskyKarafici.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ip=$(sudo nmap -p 5000 192.168.0.* |\
+ awk '
+/Nmap scan report for/{ip=$NF}
+/Synology/{print ip; exit}' )
+
+if [ -z $ip ]; then
+  echo "No Synology driver detected on  192.168.0.\*"
+else
+    echo "mount  $ip:/volume1/DATA /mnt/DiskyKarafici"
+     sudo mount  $ip:/volume1/DATA /mnt/DiskyKarafici
+fi
