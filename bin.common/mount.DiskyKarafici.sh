6abbee251cc60125b99078872b34ddc9de477da5
Karaf <karafiat@fit.vutbr.cz>
2021-04-02 10:03:38 +0200

    Update IP for mountint Synology

--- bin.common/mount.DiskyKarafici.sh
+++ bin.common/mount.DiskyKarafici.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ip=$(sudo nmap -p 5000 192.168.0.* |\
+#ip=$(sudo nmap -p 5000 192.168.15.* |\
+ip=$(sudo nmap -p 5000 192.168.1.* |\
  awk '
 /Nmap scan report for/{ip=$NF}
 /Synology/{print ip; exit}' )
 
 if [ -z $ip ]; then
-  echo "No Synology driver detected on  192.168.0.\*"
+  echo "No Synology driver detected on  192.168.1.\*"
 else
     echo "mount  $ip:/volume1/DATA /mnt/DiskyKarafici"
      sudo mount  $ip:/volume1/DATA /mnt/DiskyKarafici
